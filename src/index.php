9597efe4dcfebf03269608230d505825637b4699
til-schneider <github@murfman.de>
2015-12-22 22:28:34 +0100

    Moved index.php code in function in order to prevent pollution of the global namespace (because former global variables are local now)

--- src/index.php
+++ src/index.php
@@ -1,40 +1,42 @@
 <?php
 
-ini_set('display_errors', 1);
+function init() {
+    ini_set('display_errors', 1);
 
-// Split URI              example: 'http://localhost/slim-wiki/myfolder/mypage?action=bla'
-// into $appPath          example: '/slim-wiki'
-// and $requestPathArray  example: array('myfolder', 'mypage')
+    // Split URI              example: 'http://localhost/slim-wiki/myfolder/mypage?action=bla'
+    // into $appPath          example: '/slim-wiki'
+    // and $requestPathArray  example: array('myfolder', 'mypage')
 
-$uriPathArray    = explode("/", parse_url($_SERVER['REQUEST_URI'])['path']);
-$scriptPathArray = explode("/", dirname($_SERVER['SCRIPT_NAME']));
+    $uriPathArray    = explode("/", parse_url($_SERVER['REQUEST_URI'])['path']);
+    $scriptPathArray = explode("/", dirname($_SERVER['SCRIPT_NAME']));
 
-$basePathArray = array();
-$requestPathArray = array();
-$isBasePath = true;
-foreach ($uriPathArray as $level => $uriDir) {
-    $scriptDir = isset($scriptPathArray[$level]) ? $scriptPathArray[$level] : null;
-    if ($isBasePath && $scriptDir != $uriDir) {
-        // The URI path differs from the script path here -> We arrived at the level where the app is installed
-        $isBasePath = false;
+    $basePathArray = array();
+    $requestPathArray = array();
+    $isBasePath = true;
+    foreach ($uriPathArray as $level => $uriDir) {
+        $scriptDir = isset($scriptPathArray[$level]) ? $scriptPathArray[$level] : null;
+        if ($isBasePath && $scriptDir != $uriDir) {
+            // The URI path differs from the script path here -> We arrived at the level where the app is installed
+            $isBasePath = false;
+        }
+
+        if ($isBasePath) {
+            $basePathArray[] = $uriDir;
+        } else {
+            $requestPathArray[] = $uriDir;
+        }
     }
+    $basePath = rtrim(implode('/', $basePathArray), '/') . '/';
 
-    if ($isBasePath) {
-        $basePathArray[] = $uriDir;
-    } else {
-        $requestPathArray[] = $uriDir;
+    $https = false;
+    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+        $https = true;
     }
+    $baseUrl = 'http' . ($https ? 's' : '') . '://' . $_SERVER['HTTP_HOST'] . $basePath;
+
+    require_once __DIR__ . '/server/logic/Main.php';
+
+    Main::get()->dispatch($baseUrl, $basePath, $requestPathArray);
 }
-$basePath = rtrim(implode('/', $basePathArray), '/') . '/';
 
-$https = false;
-if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
-    $https = true;
-}
-$baseUrl = 'http' . ($https ? 's' : '') . '://' . $_SERVER['HTTP_HOST'] . $basePath;
-
-unset($uriPathArray, $scriptPathArray, $basePathArray, $isBasePath, $https);
-
-require_once __DIR__ . '/server/logic/Main.php';
-
-Main::get()->dispatch($baseUrl, $basePath, $requestPathArray);
+init();
