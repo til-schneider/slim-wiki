40389176cf86297e5984b9b3ce0b95d855a4bb4d
til-schneider <github@murfman.de>
2015-12-22 12:38:58 +0100

    Fixed: Selection background of editor wasn't visible

--- src/server/layout/page.php
+++ src/server/layout/page.php
@@ -9,6 +9,13 @@
 
   <base href="<?php echo $data['baseUrl']; ?>">
 
+  <?php if ($data['isEditMode']) { ?>
+  <!-- build:css client/edit.css -->
+  <link href="client/libs/CodeMirror/lib/codemirror.css" rel="stylesheet">
+  <link href="client/libs/CodeMirror/theme/railscasts.css" rel="stylesheet">
+  <!-- endbuild -->
+  <?php } // if $data['isEditMode'] ?>
+
   <!-- build:css client/view.css -->
   <!--
    | Styles see: https://highlightjs.org/static/demo/
@@ -19,13 +26,6 @@
 
   <link href=".tmp/app-view.css" rel="stylesheet" />
   <!-- endbuild -->
-
-  <?php if ($data['isEditMode']) { ?>
-  <!-- build:css client/edit.css -->
-  <link href="client/libs/CodeMirror/lib/codemirror.css" rel="stylesheet">
-  <link href="client/libs/CodeMirror/theme/railscasts.css" rel="stylesheet">
-  <!-- endbuild -->
-  <?php } // if $data['isEditMode'] ?>
 
 </head>
 <body<?php echo $data['isEditMode'] ? ' class="edit-mode"' : '' ?>>
@@ -60,12 +60,6 @@
   </div>
 </body>
 
-<!-- build:js client/view.js -->
-<script src="client/libs/highlightjs/highlight.pack.js"></script>
-
-<script src="client/js/app-view.js"></script>
-<!-- endbuild -->
-
 <?php if ($data['isEditMode']) { ?>
 <!-- build:js client/edit.js -->
 <script src="client/libs/CodeMirror/lib/codemirror.js"></script>
@@ -85,5 +79,11 @@
 <script src="client/js/app-edit.js"></script>
 <!-- endbuild -->
 <?php } // if $data['isEditMode'] ?>
+
+<!-- build:js client/view.js -->
+<script src="client/libs/highlightjs/highlight.pack.js"></script>
+
+<script src="client/js/app-view.js"></script>
+<!-- endbuild -->
 
 </html>
