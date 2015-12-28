12860e7b5bbdeefea0a0777b5c641b002bd74c04
til-schneider <github@murfman.de>
2015-12-28 18:03:06 +0100

    Fixed overflow behaviour (e.g. for small screens)

--- src/server/layout/page.php
+++ src/server/layout/page.php
@@ -105,23 +105,24 @@ if ($mode == 'edit') {
 
   if ($mode == 'view' || $mode == 'edit' || $mode == 'noSuchArticle' || $mode == 'createArticle') {
     ?><nav class="breadcrumbs"><div class="main-column"><?php
+      if ($data['showCreateUserButton']) {
+        ?><a class="btn btn-default btn-xs pull-right" href="<?php echo $data['requestPath']; ?>?createUser"><?php echo $i18n['button.createUser']; ?></a><?php
+      }
+      if ($mode == 'view' || $mode == 'noSuchArticle') {
+        ?><a class="btn btn-default btn-xs pull-right" href="<?php echo $data['requestPath']; ?>?edit"><?php echo $i18n['button.edit']; ?></a><?php
+      }
+
       $isFirst = true;
       foreach ($data['breadcrumbs'] as $item) {
         if (! $isFirst) {
           echo ' / ';
         }
         if ($item['active'] || is_null($item['path'])) {
-          echo $item['name'];
+          echo '<span>' . $item['name'] . '</span>';
         } else {
           ?><a href="<?php echo $data['basePath'] . $item['path'] . (($mode == 'edit') ? '?edit' : ''); ?>"><?php echo $item['name']; ?></a><?php
         }
         $isFirst = false;
-      }
-      if ($data['showCreateUserButton']) {
-        ?><a class="btn btn-default btn-xs pull-right" href="<?php echo $data['requestPath']; ?>?createUser"><?php echo $i18n['button.createUser']; ?></a><?php
-      }
-      if ($mode == 'view' || $mode == 'noSuchArticle') {
-        ?><a class="btn btn-default btn-xs pull-right" href="<?php echo $data['requestPath']; ?>?edit"><?php echo $i18n['button.edit']; ?></a><?php
       }
     ?></div></nav><?php
   }
